e551d2473c86897ab7f6cf6f2581b64c7ae60f82
Shasank <shasankgedda6@gmail.com>
2025-04-09 16:37:30 +0530

    Fully working code

--- Ass11/findall.c
+++ Ass11/findall.c
@@ -7,136 +7,166 @@
 #include <unistd.h>
 #include <errno.h>
 
-typedef struct{
-    uid_t uid;
-    char *login;
-}UidMapping;
+typedef struct {
+    char *username;
+    unsigned int user_id;
+} UserRecord;
 
-UidMapping *uid_map = NULL;
-int uid_map_size = 0;   
+UserRecord *user_database = NULL;
+int database_size = 0;
 
-void load_user_mappings(){
-    FILE *file = fopen("/etc/passwd","r");
-    if( !file ){
-        perror("Error on opening /etc/passwd\n");
-        return ;
+void initialize_user_database() {
+    char buffer[2048];
+    FILE *passwd = fopen("/etc/passwd", "r");
+    
+    if (!passwd) {
+        fprintf(stderr, "Cannot access user database: %s\n", strerror(errno));
+        return;
     }
-
-    char line[1024];
 
     /*
         the structure of every line is 
             username:x:UID:GID:comment:home_directory:shell
     */
-
-    while(fgets(line,sizeof(line),file)){
-        char *username = strtok(line,":");
-
-        if( !username ) continue;
-
-        strtok(NULL,":");   // skip passwork field
-
-        char *uid_str = strtok(NULL,":");
-        if( !uid_str ) continue;
-
-        uid_t uid = atoi(uid_str);
-
-        uid_map = realloc(uid_map , (uid_map_size+1)*sizeof(UidMapping));
-        if( !uid_map ){
-            perror("Memory allocation failed\n");
-            fclose(file);
-            return ;
+    
+    while (fgets(buffer, sizeof(buffer), passwd)) {
+        char *name_ptr = buffer;
+        char *delimiter = strchr(name_ptr, ':');
+        if (!delimiter) continue;
+        
+        *delimiter = '\0';
+        char *id_start = delimiter + 1;
+        
+        delimiter = strchr(id_start, ':');
+        if (!delimiter) continue;
+        
+        id_start = delimiter + 1;
+        delimiter = strchr(id_start, ':');
+        if (!delimiter) continue;
+        
+        *delimiter = '\0';
+        unsigned int id_value = atoi(id_start);
+        
+        int new_size = database_size + 1;
+        UserRecord *temp = realloc(user_database, new_size * sizeof(UserRecord));
+        
+        if (!temp) {
+            fprintf(stderr, "Failed to allocate memory\n");
+            fclose(passwd);
+            return;
         }
-
-        uid_map[uid_map_size].uid = uid;
-        uid_map[uid_map_size].login = strdup(username);
-        uid_map_size++;
-    }
-
-    fclose(file);
-}
-
-const char *get_login_from_uid(uid_t uid ){
-    for( int i=0 ; i<uid_map_size ; i++ ){
-        if( uid_map[i].uid == uid ){
-            return uid_map[i].login;
-        }
-    }
-
-    struct passwd *pw = getpwuid(uid);
-    if (pw) {
-        return pw->pw_name;
+        
+        user_database = temp;
+        user_database[database_size].username = strdup(name_ptr);
+        user_database[database_size].user_id = id_value;
+        database_size = new_size;
     }
     
-    static char uid_str[32];
-    sprintf(uid_str, "%u", uid);
-    return uid_str;
+    fclose(passwd);
 }
 
-int has_extension ( const char *filename , const char *ext ){
-    const char *dot = strrchr(filename,'.');
-    if( !dot || dot == filename ) return 0;
-    return strcmp(dot+1,ext) == 0;
+char* find_username(unsigned int id) {
+    static char id_buffer[64];
+    
+    for (int index = 0; index < database_size; index++) {
+        if (user_database[index].user_id == id) {
+            return user_database[index].username;
+        }
+    }
+    
+    struct passwd *pwd_entry = getpwuid(id);
+    if (pwd_entry && pwd_entry->pw_name) {
+        return pwd_entry->pw_name;
+    }
+    
+    snprintf(id_buffer, sizeof(id_buffer), "%u", id);
+    return id_buffer;
 }
 
-void search_directory( const char *dir_path , const char *ext , int *count ){
-    DIR *dir = opendir(dir_path);
-    if (!dir) {
-        fprintf(stderr, "Error opening directory %s: %s\n", dir_path, strerror(errno));
+int check_file_type(const char *name, const char *suffix) {
+    size_t name_len = strlen(name);
+    size_t suffix_len = strlen(suffix);
+    
+    if (name_len <= suffix_len) return 0;
+    
+    const char *file_ext = name + (name_len - suffix_len);
+    if (*(file_ext - 1) != '.') return 0;
+    
+    return strcasecmp(file_ext, suffix) == 0;
+}
+
+void explore_files(const char *base_path, const char *file_type, int *matches) {
+    struct dirent *item;
+    struct stat file_info;
+    char full_path[4096];
+    
+    DIR *folder = opendir(base_path);
+    if (!folder) {
+        fprintf(stderr, "Cannot access directory %s: %s\n", base_path, strerror(errno));
         return;
     }
+    
+    while ((item = readdir(folder))) {
 
-    struct dirent *entry;
-    while((entry = readdir(dir)) != NULL ){
-        if( strcmp(entry->d_name,".") == 0 || strcmp(entry->d_name,"..") == 0){
-            continue; // in question they gave not to recurse on these 
-        }
-
-        char path[PATH_MAX];
-        snprintf(path , PATH_MAX , "%s/%s",dir_path , entry->d_name);
-
-        struct stat st;
-        if( lstat(path,&st)==-1){
-            fprintf(stderr,"Error accessing %s: %s\n",path,strerror(errno));
+        if (item->d_name[0] == '.' && 
+            (item->d_name[1] == '\0' || 
+             (item->d_name[1] == '.' && item->d_name[2] == '\0'))) {
             continue;
         }
-
-        if(S_ISDIR(st.st_mode)){
-            search_directory(path,ext,count);
-        }else if(S_ISREG(st.st_mode) && has_extension(entry->d_name , ext)){
-            (*count)++;
-            printf("%-4d : %-10s %-12ld %s\n", *count, get_login_from_uid(st.st_uid),(long)st.st_size, path);
+        
+        strcpy(full_path, base_path);
+        size_t path_len = strlen(full_path);
+        
+        if (full_path[path_len-1] != '/') {
+            full_path[path_len] = '/';
+            full_path[path_len+1] = '\0';
+            path_len++;
         }
-
+        
+        strcat(full_path, item->d_name);
+        
+        if (lstat(full_path, &file_info) != 0) {
+            fprintf(stderr, "Cannot get info for %s: %s\n", full_path, strerror(errno));
+            continue;
+        }
+        
+        if (S_ISREG(file_info.st_mode)) {
+            if (check_file_type(item->d_name, file_type)) {
+                (*matches)++;
+                printf("%-4d : %-10s %-12ld %s\n", 
+                       *matches, 
+                       find_username(file_info.st_uid),
+                       (long)file_info.st_size, 
+                       full_path);
+            }
+        } else if (S_ISDIR(file_info.st_mode)) {
+            explore_files(full_path, file_type, matches);
+        }
     }
-
-    closedir(dir);
+    
+    closedir(folder);
 }
 
-int main( int argc , char *argv[]){
-    if( argc != 3 ){
-        fprintf(stderr,"usage: %s <directory> <extension>\n",argv[0]);
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Usage: %s <directory> <file_extension>\n", argv[0]);
         return 1;
     }
-
-    char *dir_name = argv[1];
-    char *extension = argv[2];
-
-    //load user mappings from /etc/passwd
-    load_user_mappings();
-
-    printf("NO   : OWNER      SIZE         NAME\n");
-    printf("--   : -----      ----         ----\n");
-
-    int count = 0;
-    search_directory(dir_name , extension , &count);
-
-    printf("+++ %d files match the extension %s\n", count, extension);
-
-    for (int i = 0; i < uid_map_size; i++) {
-        free(uid_map[i].login);
+    
+    initialize_user_database();
+    
+    printf("%-4s : %-10s %-12s %s\n", "NO", "OWNER", "SIZE", "NAME");
+    printf("%-4s : %-10s %-12s %s\n", "---", "----", "-----", "--------");
+    
+    int result_count = 0;
+    explore_files(argv[1], argv[2], &result_count);
+    
+    printf("+++ %d files match the extension %s\n", result_count, argv[2]);
+    
+    for (int i = 0; i < database_size; i++) {
+        free(user_database[i].username);
     }
-    free(uid_map);
+    free(user_database);
     
     return 0;
 }
